c02f4c6eaf21c05608a2cabc7307dbb46d6bf0b7
Leigh Stillard <leigh@stillard.com>
2022-02-16 11:29:35 +1100

    Finished sync chapter. Go vet is awesome!

--- sync/sync.go
+++ sync/sync.go
@@ -1,10 +1,19 @@
 package syncDemo
 
+import "sync"
+
 type Counter struct {
+	mu    sync.Mutex
 	value int
 }
 
+func newCounter() *Counter {
+	return &Counter{}
+}
+
 func (c *Counter) Inc() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
 	c.value++
 }
 
