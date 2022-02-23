81aaa004e6c5ee224934a8c83c82df1e6e6dddb2
Leigh Stillard <leigh@stillard.com>
2022-02-23 15:15:36 +1100

    Calling it for the afternoon. These io refactors are making my head spin

---
+++ json/file_system_store.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"encoding/json"
+	"os"
+)
+
+type FileSystemPlayerStore struct {
+	database *json.Encoder
+	league   League
+}
+
+func NewFileSystemPlayerStore(file *os.File) *FileSystemPlayerStore {
+	file.Seek(0, 0)
+	league, _ := NewLeague(file)
+
+	return &FileSystemPlayerStore{
+		database: json.NewEncoder(&tape{file}),
+		league:   league,
+	}
+}
+
+func (f *FileSystemPlayerStore) GetLeague() League {
+	return f.league
+}
+
+func (f *FileSystemPlayerStore) GetPlayerScore(name string) int {
+
+	player := f.league.Find(name)
+
+	if player != nil {
+		return player.Wins
+	}
+
+	return 0
+}
+
+func (f *FileSystemPlayerStore) RecordWin(name string) {
+	player := f.league.Find(name)
+
+	if player != nil {
+		player.Wins++
+	} else {
+		f.league = append(f.league, Player{name, 1})
+	}
+
+	f.database.Encode(f.league)
+}
